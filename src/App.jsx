8ab1fda9f3aee13ced8ca401a560baf01d6b8b00
Raxuis <raclot.raphael@gmail.com>
2024-01-08 21:02:56 +0100

    Edited Form.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Form from "./components/Form";
 
+function QueryApi() {
+  const fetchGame = async () => {
+    const response = await fetch(`/api/games?q=${query}`);
+    const data = await response.json();
+  };
+}
 function App() {
   return (
     <>
