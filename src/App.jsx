e83268dd04bd80dd22889af86aed027fdab98f2f
Raxuis <raclot.raphael@gmail.com>
2024-01-09 10:06:10 +0100

    Edited : Form.jsx (api query)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 import Form from "./components/Form";
-import LandingText from "./components/LandingText";
 
 function App() {
-  /* const [gameInfo, setGameInfo] = useState(null);
-
-  const fetchGame = async (query) => {
-    try {
-      const response = await axios.get(
-        `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean`
-      );
-      setGameInfo(response.data);
-    } catch (error) {
-      console.error("Error fetching game:", error);
-      setGameInfo(null);
-    }
-  }; */
-
   return (
     <>
       <Form />
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,65 +1,41 @@
 import React from "react";
 import LandingText from "./LandingText";
+import axios from "axios";
+
+function submitButton(event, amount, difficulty, setQuestions, questions) {
+  event.preventDefault();
+  const url = `https://opentdb.com/api.php?amount=${amount}&category=15&difficulty=${difficulty}&type=boolean`;
+  axios
+    .get(url)
+    .then((response) => {
+      questions = response.data.results;
+      setQuestions(questions);
+      console.log(questions);
+    })
+
+    .catch((error) => {
+      console.error("Error fetching questions:", error);
+    });
+}
 
 const Form = () => {
-  const [query, setQuery] = React.useState("");
-  const submitButtonRef = React.useRef();
+  const [amount, setAmount] = React.useState(5);
+  const [difficulty, setDifficulty] = React.useState("");
+  const [questions, setQuestions] = React.useState([]);
+
+  const handleChange = (setStateFunc, value) => {
+    setStateFunc(value || "facile");
+  };
 
   return (
     <div className="mx-auto mt-5 max-w-lg sm-w-full px-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
       <LandingText />
-      <form className="flex items-center flex-col p-10 gap-3 ">
-        {/* <div className="relative w-full">
-          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </div>
-          <input
-            onChange={(e) => {
-              setQuery(e.target.value);
-            }}
-            type="text"
-            id="voice-search"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Video Games"
-            required
-          ></input>
-        </div>
-        <button
-          type="submit"
-          id="submit-button"
-          ref={submitButtonRef}
-          className={`items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 
-          ${query ? "inline-flex" : "hidden"}`}
-        >
-          <svg
-            className="mr-2 -ml-1 w-5 h-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-          Search
-        </button>
-      </form>
-          <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>*/}
+      <form
+        className="flex items-center flex-col p-10 gap-3"
+        onSubmit={(e) => {
+          submitButton(e, amount, difficulty, setQuestions, questions);
+        }}
+      >
         <label htmlFor="amount" name="amount">
           Choisissez combien de questions vous souhaitez :
         </label>
@@ -67,30 +43,38 @@ const Form = () => {
           name="amount"
           id="amount"
           className="block w-full px-3 py-2 mt-1 text-base text-white-900 bg-white border border-gray-300 rounded-md dark:bg-gray-700"
+          onChange={(e) => handleChange(setAmount, e.target.value)}
         >
-          {" "}
-          <option value="5">5</option>
-          <option value="10">10</option>
-          <option value="15">15</option>
-          <option value="20">20</option>
-          <option value="25">25</option>
-          <option value="30">30</option>
-          <option value="35">35</option>
-          <option value="40">40</option>
-          <option value="45">45</option>
-          <option value="50">50</option>
+          {[5, 10, 15, 20, 25, 30, 35, 40, 45, 50].map((value) => (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          ))}
         </select>
         <label htmlFor="difficulty">Sélectionnez votre difficulté :</label>
         <select
           name="difficulty"
           id="difficulty"
           className="block w-full px-3 py-2 mt-1 text-base text-white-900 bg-white border border-gray-300 rounded-md dark:bg-gray-700"
+          onChange={(e) => handleChange(setDifficulty, e.target.value)}
         >
-          {" "}
-          <option value="">N'importe quelle difficulté</option>
-          <option value="easy">Facile</option>
-          <option value="medium">Moyen</option>
-          <option value="hard">Difficile</option>
+          <option
+            key={"any"}
+            value={
+              Math.random() < 0.33
+                ? "facile"
+                : Math.random() < 0.66
+                ? "moyen"
+                : "difficile"
+            }
+          >
+            N'importe quelle difficulté
+          </option>
+          {["facile", "moyen", "difficile"].map((value) => (
+            <option key={value} value={value}>
+              {value.charAt(0).toUpperCase() + value.slice(1)}
+            </option>
+          ))}
         </select>
         <button
           type="submit"
